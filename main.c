7facdf1ee6033748d50c2d992f4c4e63ce19ee38
Irri <irri96@live.cl>
2018-04-05 16:00:51 -0300

    aloalo

--- main.c
+++ main.c
@@ -3,11 +3,16 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define MAXCOM 10
+#define MAXARG 20
+
 typedef struct{
-	char *name,*arguments[];	//comando
-}command;
+	char *args[MAXARG];	//maximo 20 argumentos
+	int narg;
+}comando;
 
 int main(){
+	int i,j,k;
 	char buffer[1024];
 	char *ctok,*atok,*cptr,*aptr;
 	char user[50];
@@ -15,23 +20,46 @@ int main(){
 	char hostname[50];
 	size_t hsize = 50;
 	getlogin_r(user,usize);
-	command comandos[10];		//almacenar comandos con sus nombres para ejecutar
+	comando comandos[MAXCOM];		//almacenar comandos 
+	for(i=0;i<MAXCOM;i++){
+		for(j=0;j<MAXARG;j++){
+			comandos[i].args[j]=(char*)malloc(50);
+		}
+	}
+	int ncom;
 	char cwd[100];
 	gethostname(hostname,hsize);
 	while(1){
+		ncom = 0;					//numero de comando de linea
 		getcwd(cwd,100);
 		printf("[%s@%s][ %s ](OwO)>> ",user,hostname,cwd);
 		fgets(buffer,1024,stdin);
 		ctok = strtok_r(buffer,"|\n",&cptr);
 		while(ctok!=NULL){
-			printf("comando: %s\n",ctok);	//separo las weas de argumentos maldita sea
+			//printf("comando: %s\n",ctok);	//separo argumentos 
+			int narg = 0;
 			atok = strtok_r(ctok," ",&aptr);
 			while(atok!=NULL){
-				printf(" argumento: (%s)\n",atok);
+				//printf(" argumento: (%s)\n",atok);
+				//printf("%d\n",narg);
+				strcpy(comandos[ncom].args[narg],atok);
+				//printf("chao\n");
+				narg++;
 				atok = strtok_r(NULL," \n\0",&aptr);
 			}
+			comandos[ncom].narg=narg;
+			ncom++;
 			ctok = strtok_r(NULL,"|\n",&cptr);
 		}
+		/*
+		for(i=0;i<ncom;i++){
+			printf("comando %d:\n",i);
+			int narg = comandos[i].narg;
+			for(j=0;j<narg;j++){
+				printf("%s\n",comandos[i].args[j]);
+			}
+		}
+		*/
 	}
 	return 0;
 }
