3f8057c94a7306e8b604ebcf0ee701702b077ea3
Irri <irri96@live.cl>
2018-04-05 09:46:53 -0300

    parseo

--- main.c
+++ main.c
@@ -1,23 +1,37 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
 typedef struct{
-	char *name,**arguments;	//comando, arreglo de argumentos
+	char *name,*arguments[];	//comando
 }command;
 
 int main(){
 	char buffer[1024];
+	char *ctok,*atok,*cptr,*aptr;
 	char user[50];
 	size_t usize = 50;
 	char hostname[50];
 	size_t hsize = 50;
 	getlogin_r(user,usize);
-	char *cwd=get_current_dir_name();
+	command comandos[10];		//almacenar comandos con sus nombres para ejecutar
+	char cwd[100];
 	gethostname(hostname,hsize);
-	printf("%s",cwd);
 	while(1){
-		printf("[%s@%s](UwU)>> ",user,hostname);
+		getcwd(cwd,100);
+		printf("[%s@%s][ %s ](OwO)>> ",user,hostname,cwd);
 		fgets(buffer,1024,stdin);
+		ctok = strtok_r(buffer,"|\n",&cptr);
+		while(ctok!=NULL){
+			printf("comando: %s\n",ctok);	//separo las weas de argumentos maldita sea
+			atok = strtok_r(ctok," ",&aptr);
+			while(atok!=NULL){
+				printf(" argumento: (%s)\n",atok);
+				atok = strtok_r(NULL," \n\0",&aptr);
+			}
+			ctok = strtok_r(NULL,"|\n",&cptr);
+		}
 	}
 	return 0;
 }
